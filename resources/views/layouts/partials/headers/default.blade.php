560b1df57f857bee1ade2c11838bd201da0a2d62
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2019-07-19 20:14:44 -0300

    Correção no header

--- resources/views/layouts/partials/headers/default.blade.php
+++ resources/views/layouts/partials/headers/default.blade.php
@@ -2,9 +2,9 @@
     <!-- Logo -->
     <a href="{{url('/')}}" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>MPRN</b></span>
+        <span class="logo-mini"><b>EB</b></span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>Geo</b>SUS</span>
+        <span class="logo-lg"><b>E</b>legante <b>B</b>ranco</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
